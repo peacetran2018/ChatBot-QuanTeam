dd3e2089f92c0d80fc147c74cd5c594549404b58
Peace Tran <peacetran2018@gmail.com>
2019-06-26 10:00:17 +0700

    Update server file

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
