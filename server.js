cbe8a3dd3b6b72a7743377d62dd465b808c04833
Peace Tran <peacetran2018@gmail.com>
2019-06-26 10:04:24 +0700

    add prefix

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/chat-bot-app'));
 
 // Send all requests to index.html
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/chat-bot-app/index.html'));
+});
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
